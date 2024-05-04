cc25e6ce64cca02e54f50bb9860e443c70a2687d
samsonssali <ssali@jitpomi.com>
2024-05-04 13:40:05 -0600

    illustrate immutability - essential for writing pure functions

---
+++ immutable.js
@@ -0,0 +1,3 @@
+export const indexes = Object.freeze([0, 1, 2, 3, 4, 5]);
+
+export const addElements = (array)  =>  Object.freeze([...array])
--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+import {addElements, indexes} from './immutable.js';
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1 class="text-3xl font-bold underline text-amber-700">
-      Hello world!
+  <div class="p-5">
+    <h1 class="text-3xl mb-1 font-bold underline text-orange-700">
+      Immutability!
     </h1>
+    <p class="text-orange-950 text-sm"> Simply means it can not be changed in place (Not mutable)</p>
+    <p class="text-orange-950 text-sm"> An essential concept for writing pure functions</p>
+    <p class="text-sm mb-1 font-bold underline text-orange-700">Example</p>
+    <pre class="text-orange-950 text-sm">
+      const indexes = Object.freeze(${JSON.stringify(indexes)});
+      indexes.push(...[6,7,8,9]) // throws: <span class="text-orange-700">Uncaught TypeError: Cannot add property 6, object is not extensible</span>
+      export const addElements = (array)  =>  Object.freeze([...array]);
+      const myindexes = addElements(indexes);
+      console.log(myindexes); // prints ${addElements(indexes)}
+      const ourindexes = addElements([...myindexes,6,7,8,9]) 
+      console.log(ourindexes); // prints ${addElements([...addElements(indexes),6,7,8,9])}
+    </pre>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
