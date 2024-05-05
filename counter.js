cbb66e99f4056bfaf58d8b1acb1aa5ee40f644bf
samsonssali <ssali@jitpomi.com>
2024-05-04 18:15:45 -0600

    init use composition

--- counter.js
+++ counter.js
@@ -1,9 +1,51 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+import {pipe} from './composition.js';
+
+export const selectDomElement = (selector) => {
+  return () =>{
+    return document.querySelector(selector);
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+};
+
+
+export const addValue = (valueToAdd) => {
+  return (element) =>{
+    element.innerHTML = parseInt(element.innerText)+valueToAdd;
+    return element;
+  }
+};
+
+export const addValueToElementContent = (selector, valueToAdd) => {
+   return (element) =>{
+     pipe(
+       selectDomElement(selector),
+       addValue(valueToAdd)
+     )()
+     return element;
+   }
 }
+
+export const listenToEvent = (event,action) => {
+  return(target) =>{
+    target.addEventListener(event, action);
+    return target;
+  }
+}
+
+export const eventHandler = (event, targetSelector, action) => {
+  return () =>{
+    return pipe(
+      selectDomElement(targetSelector),
+      listenToEvent(event, action)
+    )()
+  }
+}
+
+const action = pipe(
+  selectDomElement('#display'),
+  addValueToElementContent('#display',1)
+)
+
+export const setupCounter = eventHandler('click', 'button', action)
+
+
+
--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {setupCounter} from './counter.js';
+
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1 class="text-3xl font-bold underline text-amber-700">
-      Hello world!
+  <div class="p-5">
+    <h1 class="text-3xl mb-5 font-bold underline text-amber-700">
+      Use Composition!
     </h1>
+    <div class="text-red-950 "> 
+    <p>Click to add One</p>
+    <button id="button" class="rounded px-5 py-2 bg-red-700 hover:bg-red-950 hover:text-red-50 active:bg-red-700">
+    <span class="text-4xl p-2" id="display">
+    ${0}
+    </span>
+    </button>
+    </div>
+    <p>Exercise:</p>
+    <pre>
+    1. Name the impure and pure functions in the code (explaining your choice)
+    2. Following this example; Implement a countdown using compose
+   </pre>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter()
